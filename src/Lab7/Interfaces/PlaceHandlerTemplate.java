460de7a73ad49a5ac0eb52d656d6ab64d994eb7c
thomas_mark_varga <thomasvarga64@gmail.com>
2024-05-27 15:15:01 +0300

    Hopefully everything, part 2

--- src/Lab7/Interfaces/PlaceHandlerTemplate.java
+++
@@ -1,9 +0,0 @@
-package Lab7.Interfaces;
-
-public interface PlaceHandlerTemplate {
-	public void AddPlace(PlaceTemplate place);
-
-	public PlaceTemplate GetPlaceByName(String Name);
-
-	public String PrintAllPlaces();
-}
